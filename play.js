2b285e88359cb58db743bf3e09420b7c22a48bb6
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-21 21:37:45 -0700

    Add stdin for keyboard input

--- play.js
+++ play.js
@@ -1,21 +1,24 @@
-const net = require("net");
-
-// establishes a connection with the game server
-const connect = function() {
-  const conn = net.createConnection({
-    host: "172.20.10.2",
-    port: 50541
-  });
-  
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  
-  conn.on("data", (data) => {
-    console.log(`Server says: ${data}`);
-  });
-
-  return conn;
-};
+const connect = require("./client");
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  const handleUserInput = function() {
+    stdin.on("data", (key) => {
+      // \u0003 maps to ctrl+c input
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  };
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+setupInput();
