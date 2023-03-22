9f348ae54b14376022295f873b8e17056f57adb5
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-21 21:52:42 -0700

    Refactor to add input functions to their own file

---
+++ input.js
@@ -0,0 +1,21 @@
+// setup interface to handle user input from stdin
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  const handleUserInput = function() {
+    stdin.on("data", (key) => {
+      // \u0003 maps to ctrl+c input
+      if (key === '\u0003') {
+        process.exit();
+      }
+    });
+  };
+
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+module.exports = { setupInput };
