29f65d68a5bcfe89537d8331494252b8684db994
Kevin Heaslip <kevin.heaslip@mailbox.org>
2023-03-25 13:57:02 -0700

    Add handleUserInput to send key presses to server for snake movement

--- input.js
+++ input.js
@@ -1,5 +1,9 @@
+// Stores the active TCP connection object.
+let connection;
+
 // setup interface to handle user input from stdin
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
@@ -12,10 +16,40 @@ const setupInput = function() {
         process.exit();
       }
     });
+  
+    stdin.on("data", (key) => {
+      if (key === 'w') {
+        // console.log("Move: up");
+        connection.write("Move: up");
+      }
+    });
+  
+    stdin.on("data", (key) => {
+      if (key === 'a') {
+        // console.log("Move: left");
+        connection.write("Move: left");
+      }
+    });
+  
+    stdin.on("data", (key) => {
+      if (key === 's') {
+        // console.log("Move: down");
+        connection.write("Move: down");
+      }
+    });
+  
+    stdin.on("data", (key) => {
+      if (key === 'd') {
+        // console.log("Move: right");
+        connection.write("Move: right");
+      }
+    });
+  
   };
-
+  
   stdin.on("data", handleUserInput);
   return stdin;
 };
+
 
 module.exports = { setupInput };
